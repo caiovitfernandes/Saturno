e3230709fda4dc2869bb642717c6ccef9ef557e4
Daniel Benevides <danielfbenevides@gmail.com>
2022-06-02 14:31:57 -0300

    Mais opções de conversa com o Chatbot

--- FrontEnd/javascript/responses.js
+++ FrontEnd/javascript/responses.js
@@ -5,24 +5,24 @@ function getBotResponse(input) {
     if (input.includes("tarefa")) {
         return "Logo acima você encontrará a área de tarefas, nela você poderá criar, editar ou apagar suas tarefas."
     }
-    else if (input.includes("conquista")) {
-        return "Ao criar e concluir tarefas você desbloqueia conquistas que podem ser vistas na aba de conquistas."
-    }
     else if (input.includes("configurações") || input.includes("conta") || input.includes("email")
     || input.includes("configuracoes")) {
         return "As configurações podem ser acessadas clicando no botão encontrado no canto superior direito desta página. Lá você poderá alterar suas informações de conta, como seu email e senha."
     }
+    else if (input.includes)
 
     //respostas simples
-    if (input == "ola" || input == "oi") {
+    if (input == "ola" || input == "oi" || input.includes("oi")) {
         return "Olá!";
     } else if (input == "tchau" || input == "xau") {
         return "Até mais!";
     } else if (input == "obrigado" || input == "valeu" || input == "obg" || input == "vlw") {
         return "Foi um prazer poder te ajudar :)"
+    } else if (input.includes("chama")  || input.includes("nome")) {
+        return "Eu não possuo um nome, fui criado com o único propósito de guiar os usuários do Saturno no caminho da organização pessoal e o fim da procastinação"
+    } else if (input.includes("idade") || input.includes("anos")) {
+        return "Fui criado em abril de 2022"
     }
-
-    
     
     else {
         return "Não entendi.";
